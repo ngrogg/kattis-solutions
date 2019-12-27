1935ad90700a65bb90ec0afa941e637adefce27a
Nicholas Grogg <oddone9139@OldBoy>
2019-12-26 21:53:22 -1000

    Added my solution to the 'Stuck In A Time Loop' problem

---
+++ GroggTimeLoop.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int loops;
+	cin >> loops;
+
+	for(int i = 1; i <= loops; i++){
+		cout << i << " Abracadabra" << endl;
+	}
+}
