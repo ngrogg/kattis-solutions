35a85409fa401e943e29f767ca244265359366fe
Oddone330 <Nicholas_Grogg6505@hotmail.com>
2020-01-17 13:24:37 -1000

    Added my solution to the problem R2

---
+++ GroggR2.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	int R1, R2, S;
+	cin >> R1 >> S;
+	R2 = (2*S) - R1;
+	cout << R2 << endl;
+
+	return 0;
+}
