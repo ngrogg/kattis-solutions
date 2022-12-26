c9c94c550cdf7954e73df9ed07cc4bd6c0c69efd
Nicholas Grogg <ngrogg@pop-os.localdomain>
2022-12-25 22:51:56 -1000

    Added solution for Oddities

--- rust/GroggOddities/GroggOddities.rs
+++ rust/GroggOddities/GroggOddities.rs
@@ -1,20 +1,45 @@
 // Import statements 
-use std:io; 
+use std::io; 
 
+// Main function to run program
 fn main() {
-    // Keep main minimal
+    // Keep main minimal, run everything in it's own function
     runProgram();
 }
 
 // Function to run program
 fn runProgram(){
-    // Variable to hold user value 
-    let mut inputNum = String::new();
-    // Read in value 
+    // Read in number of iterations
+    let mut numIt = String::new();
+
+    // Read in value for numIt
     io::stdin()
-        .read_line(&mut inputNum);
+        .read_line(&mut numIt)
         .expect("Failed to read input");
-    // Check if number odd/even
-    // TODO
+
+    // Transmute input string to int
+    let transmuteIt: i32 = numIt.trim().parse().expect("Input not an integer");
+
+    // For loop to iterate from 0 to max iterator value
+    for i in 0..transmuteIt{
+        // Variable to hold user value 
+        let mut inputNum = String::new();
+
+        // Read in value for input number
+        io::stdin()
+            .read_line(&mut inputNum)
+            .expect("Failed to read input");
+
+        // Transmute input string to int
+        let transmuteInput: i32 = inputNum.trim().parse().expect("Input not an integer");
+
+        // Check if number odd/even
+        if transmuteInput%2==0{
+            println!("{} is even",transmuteInput);
+        }
+        else{
+            println!("{} is odd",transmuteInput);
+        }
+    }
 }
 
